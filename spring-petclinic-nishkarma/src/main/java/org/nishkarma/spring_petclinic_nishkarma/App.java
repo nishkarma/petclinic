26aed85a966ce766ebffa6a5af43920c0ff164f0
nishkarma <freelsj0@gmail.com>
2016-01-23 16:04:07 +0900

    init

---
+++ spring-petclinic-nishkarma/src/main/java/org/nishkarma/spring_petclinic_nishkarma/App.java
@@ -0,0 +1,13 @@
+package org.nishkarma.spring_petclinic_nishkarma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
